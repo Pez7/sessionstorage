4ea89a366a1c5ec7683055b6d26ac2b61a72640d
Maria Paz <maria.pazrojasc@gmail.com>
2017-06-16 13:53:08 -0400

    arreglo de errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,14 @@ function recoverData(){
 	for(var i= 0; i < sessionStorage.length; i++){
 		var name = sessionStorage.key(i);
 		var clave = sessionStorage.getItem(name);
-		document.getElementById("data").innerHTML = '<div>' + 'Nombre: ' + name + '<br/> Clave: ' + clave
- + '</div>'
+		document.getElementById("data").innerHTML += '<div>' + 'Nombre: ' + name + '<br> Clave: ' + clave + '</div>';
  }
 }
 
 function cleanData(){
-	document.getElementById("data").innerHTML = 'Limpiada vista.Los datos permanecen.';
+	document.getElementById("data").innerHTML += 'Limpiada vista. Los datos permanecen.';
 }
+
 function borrarTodo(){
 	sessionStorage.clear(); recoverData();
 }
